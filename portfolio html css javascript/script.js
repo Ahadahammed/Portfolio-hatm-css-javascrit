e52483d24f88566c54be77e73a98004189fc52fe
Ahad Ahammed <ahadahammed1029@gmail.com>
2024-09-09 23:27:21 +0600

    Add files via upload

---
+++ portfolio html css javascript/script.js
@@ -0,0 +1,27 @@
+//scroll section
+
+let section = document.querySelectorAll('section');
+let navlink = document.querySelectorAllI('header nav a');
+
+window.onescroll = () =>{
+	sections.forEach(sec => {
+		let top = window.scrolly;
+		let offset = sec.offsetTop - 100;
+		let height = sec.offsetHight;
+		let id =sec.getAttribute('id');
+		
+		if(top>=offset&&top<offset+height){
+			navlink.forEach(links=>{
+				links.classList.remove('active');
+				document.querySelectorAll('header nav a[href*='+ id +'].classList.add('active'));
+			})
+		}
+	})
+	
+	//stckey header
+	let header=document.querySelector('header');
+	
+	header.classList.toggle('sticky', window.scrolly > 100);
+}
+
+alert("hello");
